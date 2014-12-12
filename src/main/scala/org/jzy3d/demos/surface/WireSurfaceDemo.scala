47999921cadb8d815930b46d18e022b455d54197
rintcius <rintcius@gmail.com>
2014-12-12 19:38:00 +0100

    upgrade to jzy3d 0.9.1

--- src/main/scala/org/jzy3d/demos/surface/WireSurfaceDemo.scala
+++ src/main/scala/org/jzy3d/demos/surface/WireSurfaceDemo.scala
@@ -1,19 +1,15 @@
 package org.jzy3d.demos.surface
 
-import java.awt.Dimension
-import org.jzy3d.chart.Chart
-import org.jzy3d.colors.Color
-import org.jzy3d.colors.ColorMapper
+import org.jzy3d.chart.factories.AWTChartComponentFactory
+import org.jzy3d.colors.{Color, ColorMapper}
 import org.jzy3d.colors.colormaps.ColorMapRainbow
-import org.jzy3d.demos.AbstractDemo
-import org.jzy3d.demos.Launcher
-import org.jzy3d.maths.Range
-import org.jzy3d.plot3d.builder.Builder
-import org.jzy3d.plot3d.builder.Mapper
+import org.jzy3d.demos.{AbstractDemo, Launcher}
+import org.jzy3d.maths.{Dimension, Range}
+import org.jzy3d.plot3d.builder.{Builder, Mapper}
 import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid
 import org.jzy3d.plot3d.primitives.Shape
 import org.jzy3d.plot3d.rendering.canvas.Quality
-import org.jzy3d.plot3d.rendering.legends.colorbars.ColorbarLegend
+import org.jzy3d.plot3d.rendering.legends.colorbars.AWTColorbarLegend
 
 object WireSurfaceDemo {
   def main(args: Array[String]) {
@@ -27,15 +23,15 @@ class WireSurfaceDemo extends AbstractDemo {
     val mapper: Mapper = (x: Double, y: Double) => 10 * math.sin(x / 10) * math.cos(y / 20) * x
     val range = new Range(-150, 150)
     val steps = 50
-    val surface: Shape = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper).asInstanceOf[Shape]
+    val surface: Shape = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper)
     import surface._
     setColorMapper(new ColorMapper(new ColorMapRainbow, getBounds.getZmin, getBounds.getZmax, new Color(1, 1, 1, .5f)))
     setFaceDisplayed(true)
     setWireframeDisplayed(true)
     setWireframeColor(Color.BLACK)
-    chart = new Chart(Quality.Advanced)
+    chart = AWTChartComponentFactory.chart(Quality.Advanced, "newt")
     chart.getScene.getGraph.add(surface)
-    val cbar = new ColorbarLegend(surface, chart.getView.getAxe.getLayout)
+    val cbar = new AWTColorbarLegend(surface, chart.getView.getAxe.getLayout)
     cbar.setMinimumSize(new Dimension(100, 600))
     surface.setLegend(cbar)
   }
